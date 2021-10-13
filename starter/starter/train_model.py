527de259a8b19cfa49f8c11e37e64a90492c8b6c
Gaylord MARVILLE <gaylord.marville@gmail.com>
2021-10-13 18:44:16 +0200

    Fix flake8 coding-style issues

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,10 +3,14 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+import pandas as pd
+from .ml.data import process_data
 
 # Add code to load in the data.
+data = pd.read_csv("../data/census.csv")
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
+# Optional enhancement, use K-fold cross validation instead
+# of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
