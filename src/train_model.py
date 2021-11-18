000d5bf977aa52e07991af7a8db12500fdf6d4cf
Gaylord <gmarville@ltutech.com>
2021-11-18 17:18:33 +0100

    Update main.py to implement GET and POST and fix the inference pipeline of the POST request

--- src/train_model.py
+++ src/train_model.py
@@ -19,11 +19,12 @@ logging.basicConfig(
 parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 data_path = os.path.join(parent_dir, "data/clean_census.csv")
-model_fd_path = os.path.join(parent_dir, "model/")
+model_path = os.path.join(parent_dir, "model/best_model.joblib")
+encoder_path = os.path.join(parent_dir, "data/encoder.joblib")
+label_binarizer_path = os.path.join(parent_dir, "data/label_binarizer.joblib")
+scaler_path = os.path.join(parent_dir, "data/scaler.joblib")
 
 logging.info(f"data_path: {data_path}")
-
-# data_path = "../data/clean_census.csv"
 
 # Add code to load in the data.
 data = load_data(data_path)
@@ -44,29 +45,46 @@ cat_features = [
 ]
 
 # Preprocess the entire dataset for k-fold validation.
-X_train, y_train, encoder, lb = process_data(train,
-                                             categorical_features=cat_features,
-                                             label="salary", training=True)
+X_train, y_train, encoder, lb, scaler = process_data(
+                                        train,
+                                        categorical_features=cat_features,
+                                        label="salary", training=True)
 
 
-X_test, y_test, _, _ = process_data(
+logging.info("Dumping encoder...")
+joblib.dump(encoder, open(encoder_path, "wb"))
+
+logging.info("Dumping label binarizer...")
+joblib.dump(lb, open(label_binarizer_path, "wb"))
+
+logging.info("Dumping scaler...")
+joblib.dump(scaler, open(scaler_path, "wb"))
+
+X_test, y_test, _, _, _ = process_data(
                             test,
                             categorical_features=cat_features,
                             label="salary",
                             training=False,
                             encoder=encoder,
-                            lb=lb)
+                            lb=lb,
+                            scaler=scaler)
 
 
 # Train and save a model.
+logging.info("Starting model training...")
 model = train_model(X_train, y_train, X_test, y_test, params_tuning=True)
+logging.info("Model training complete")
 
 # Model performance evaluation on categorical features
+logging.info("Starting model inference...")
 preds = inference(model, X_test)
+logging.info("Model training complete")
+
+logging.info("Compute model metrics...")
 precision, recall, fbeta = compute_model_metrics(y_test, preds)
+logging.info("Model metrics complete...")
 
 logging.info(f"Precision:{precision}, Recall:{recall}, F1 score:{fbeta}")
 
-model_path = os.path.join(model_fd_path, "best_model.joblib")
 logging.info(f"Dumping best model: {model_path}")
 joblib.dump(model, open(model_path, "wb"))
